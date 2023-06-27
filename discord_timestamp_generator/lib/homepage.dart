8130ab2c92fb40d0c7c0b038114fe464bddff4ef
Mary Marquardt <creationunbound@gmail.com>
2023-06-27 17:57:26 -0400

    Added date picker.

---
+++ discord_timestamp_generator/lib/homepage.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController dateController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dateController.text = "";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Discord Timestamp Generator")),
+        body: Container(
+            padding: const EdgeInsets.all(10),
+            child: Center(
+              child: TextField(
+                controller: dateController,
+                decoration: const InputDecoration(
+                    icon: Icon(Icons.calendar_today), labelText: "Enter Date"),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1971),
+                    lastDate: DateTime(2101),
+                  );
+                },
+              ),
+            )));
+  }
+}
