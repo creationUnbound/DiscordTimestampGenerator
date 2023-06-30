d89c9c6dc9d9399ce230af38ce1e46846a974705
Mary Marquardt <creationunbound@gmail.com>
2023-06-30 17:55:05 -0400

    Added option wait time.

--- discord_timestamp_generator/lib/homepage.dart
+++ discord_timestamp_generator/lib/homepage.dart
@@ -37,6 +37,7 @@ class _HomePageState extends State<HomePage> {
                     firstDate: DateTime(1970),
                     lastDate: DateTime(2100),
                   );
+                  //await Future.delayed(const Duration(milliseconds: 200));
                   if (mounted && pickedDate != null) {
                     TimeOfDay? pickedTime = await showTimePicker(
                       context: context,
