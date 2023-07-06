4f7e65cc6ed57deb5a0d0c1dd20ed8e5b41f00a2
Mary Marquardt <creationunbound@gmail.com>
2023-07-06 17:34:14 -0400

    Changed format to ListView.

--- discord_timestamp_generator/lib/home_page.dart
+++ discord_timestamp_generator/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'utility/date_time_picker.dart';
+import 'widget/date_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -27,22 +28,18 @@ class _HomePageState extends State<HomePage> {
           title: const Text("Discord Timestamp Generator"),
           backgroundColor: Theme.of(context).colorScheme.primaryContainer,
         ),
-        body: Container(
-            padding: const EdgeInsets.all(10),
-            child: Center(
-              child: TextField(
-                  controller: dateController,
-                  decoration: const InputDecoration(
-                      icon: Icon(Icons.calendar_today),
-                      labelText: "Enter Date"),
-                  readOnly: true,
-                  onTap: () async {
-                    await dateTimePicker.pickDateTime();
-                    dateController.text = DateFormat.yMd()
-                        .add_jm()
-                        .format(dateTimePicker.dateTime);
-                    // Do something with the dateTime attributes here
-                  }),
-            )));
+        body: ListView(padding: const EdgeInsets.all(10), children: [
+          TextField(
+              controller: dateController,
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.calendar_today), labelText: "Enter Date"),
+              readOnly: true,
+              onTap: () async {
+                await dateTimePicker.pickDateTime();
+                dateController.text =
+                    DateFormat.yMd().add_jm().format(dateTimePicker.dateTime);
+                // Do something with the dateTime attributes here
+              }),
+        ]));
   }
 }
--- discord_timestamp_generator/lib/widget/date_card.dart
+++ discord_timestamp_generator/lib/widget/date_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class DateCard extends StatelessWidget {
+  const DateCard({super.key});
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
